052e73c3d34288f98e19ab5c40c87509f241ec6a
Manish Jha <manishjhazyx@gmail.com>
2021-09-26 13:39:48 +0530

    linked variables to html elements

---
+++ app.js
@@ -0,0 +1,3 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-button");
