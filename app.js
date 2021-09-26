3006f89211bd681e381cb260a7c0733a27cf6a17
Manish Jha <manishjhazyx@gmail.com>
2021-09-26 15:53:27 +0530

    added complete functionality

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
+const outputSpace = document.querySelector("#output-space");
+
+checkButton.addEventListener("click", () => {
+
+    const dob = dateOfBirth.value;
+    const numToCheck = luckyNumber.value;
+    if(dob&&numToCheck){
+        const sum = calculateSum(dob);
+        if(checkIfLucky(sum, numToCheck)){
+            outputSpace.innerText = "Your Birthday is lucky for you."
+        }
+        else{
+            outputSpace.innerText = "Your Birthday is not so lucky."
+        }
+    }
+    else{
+        outputSpace.innerText = "Enter both fields";
+    }
+});
+
+calculateSum = (dob) => {
+    dob = dob.replaceAll("-", "");
+    let sumTotal = 0;
+    for(let index = 0; index < dob.length; index++){
+        sumTotal += Number(dob[index]);
+    }
+    return sumTotal;
+}
+
+checkIfLucky = (sum, num) => {
+    if(sum%num == 0){
+        return true;
+    }
+    else{
+        return false;
+    }  
+}
